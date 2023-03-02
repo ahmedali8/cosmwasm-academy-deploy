47153903f5f9dec18125b9f63db2bc4317212911
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-03-02 15:11:51 +0500

    deploy contracts

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This repository contains the companion material to Chapter 3 from the CosmWasm Academy course "Frontend".
 
 Check out academy.cosmwasm.com !
+
+Link to original repo: [CosmWasm/academy-deploy](https://github.com/CosmWasm/academy-deploy)
--- scripts/upload_contract.ts
+++ scripts/upload_contract.ts
@@ -17,6 +17,31 @@ async function main(): Promise<void> {
    *  We're going to upload & initialise the contract here!
    *  Check out the video course on academy.cosmwasm.com!
    */
+
+  // Get our mnemonic
+  const mnemonic = getMnemonic();
+
+  // Get a signing client
+  const { client, address } = await connect(mnemonic, malagaConfig);
+
+  // Check if given wallet has enough balance
+  let { amount } = await client.getBalance(address, malagaConfig.feeToken);
+
+  // Call a faucet
+  if (amount === "0") {
+    console.warn("Not enough tokens on given wallet. Calling faucet!");
+    await hitFaucet(address, malagaConfig.feeToken, malagaConfig.faucetUrl);
+
+    let { amount } = await client.getBalance(address, malagaConfig.feeToken);
+    console.log(`New balance of ${address} is ${amount}`);
+  }
+
+  // upload the contract
+  const codeId = await uploadContracts(client, address, contracts);
+
+  // instantiate the contract
+  const contractAddress = await initToken(client, address, codeId.cw20_base);
+  console.log(contractAddress); // wasm1gjls6f8mkgc5xmdsfu0eug6eenqedxfxuep8h0hxp944eqwtaxesawrljd
 }
 
 main().then(
